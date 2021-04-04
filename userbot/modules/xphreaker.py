d01f9e3adc40c95d8932adba2ef1798138492a82
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-04 18:03:42 +0700

    Update xphreaker.py

--- userbot/modules/xphreaker.py
+++ userbot/modules/xphreaker.py
@@ -22,7 +22,7 @@ async def _(event):
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             jemboed = await conv.send_message(f"/gen {query}")
-            await asyncio.sleep(4)
+            await asyncio.sleep(8)
             asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
@@ -68,7 +68,7 @@ async def _(event):
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             jemboed = await conv.send_message(f"/bin {query}")
-            await asyncio.sleep(5)
+            await asyncio.sleep(10)
             asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
@@ -91,7 +91,7 @@ async def _(event):
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             jemboed = await conv.send_message(f"/bin {query}")
-            await asyncio.sleep(5)
+            await asyncio.sleep(10)
             asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
