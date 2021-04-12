22f31b89be41980ed80c7d48d413a52398800fcc
ximfine <xbotprojects@gmail.com>
2021-04-12 17:59:45 +0000

    pylint: auto fixes
    Signed-off-by: ximfine <xbotprojects@gmail.com>

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -37,7 +37,7 @@ async def help(event):
             string += "`" + str(i)
             string += "`\t• "
         h = await event.reply(f"•{string}•"
-                          "\n╾─────────────────────╼")
+                              "\n╾─────────────────────╼")
         await asyncio.sleep(100)
         await h.delete()
         await event.delete()
