fb9888d12153b7825847943b509eda40faa5ac3b
ImFine <79558474+ximfine@users.noreply.github.com>
2021-04-24 23:36:48 +0700

    Add json

--- userbot/modules/eval.py
+++ userbot/modules/eval.py
@@ -5,12 +5,15 @@
 #
 
 import asyncio
+import os
+import pybase64
 from os import remove
 from sys import executable
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, TERM_ALIAS
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, TERM_ALIAS, bot
 from userbot.events import register
 
+MAX_MESSAGE_SIZE_LIMIT = 4095
 
 @register(outgoing=True, pattern=r"^\.eval(?: |$)(.*)")
 async def evaluate(query):
@@ -188,10 +191,39 @@ async def terminal_runner(term):
         )
 """
 
+@register(outgoing=True, pattern="^.json$")
+async def _(event):
+    if event.fwd_from:
+        return
+    the_real_message = None
+    reply_to_id = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        the_real_message = previous_message.stringify()
+        reply_to_id = event.reply_to_msg_id
+    else:
+        the_real_message = event.stringify()
+        reply_to_id = event.message.id
+    if len(the_real_message) > MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(the_real_message)) as out_file:
+            out_file.name = "json.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    else:
+        await event.edit("{}".format(the_real_message))
+
 CMD_HELP.update({"eval": ">`.eval 2 + 3`"
                  "\nUsage: Evalute mini-expressions.",
                  "exec": ">`.exec print('hello')`"
                  "\nUsage: Execute small python scripts.",
                  "term": ">`.term <cmd>`"
                  "\nUsage: Run bash commands and scripts on your server.",
+                 "json": ">`.json <reply>`"
+                 "\nUsage: to get details of that message in json format.",
                  })
