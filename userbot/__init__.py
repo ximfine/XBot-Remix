856618e400d00bf9789203afbde7db36c07335a7
ximfine <xbotprojects@gmail.com>
2021-05-13 07:07:14 +0000

    pylint: auto fixes
    Signed-off-by: ximfine <xbotprojects@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -396,7 +396,7 @@ with bot:
             else:
                 reply_pop_up_alert = f"Silahkan Deploy XBOT-REMIX\nUntuk menggunakan bot seperti ini!"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-            
+
         @tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(rb"helpme_prev\((.+?)\)")
