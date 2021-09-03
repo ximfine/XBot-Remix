401738c96c0e94a4452f0368e2c09116c37f792e
ImFine <79558474+ximfine@users.noreply.github.com>
2021-09-03 14:56:00 +0700

    Update xphreaker.py

--- userbot/modules/xphreaker.py
+++ userbot/modules/xphreaker.py
@@ -10,6 +10,39 @@ import asyncio
 from userbot import bot, CMD_HELP
 from userbot.events import register
 
+@register(outgoing=True, pattern="^.dec(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.edit("`Reply to any user message.`")
+        return
+    reply_message = await event.get_reply_message()
+    # if not reply_message.text:
+    # await event.edit("```reply to file message```")
+    # return
+    chat = "@decryptorxbot"
+    reply_message.sender
+    if reply_message.sender.bot:
+        await event.edit("`Reply to actual users message.`")
+        return
+    await event.edit("`Decrypting Config...`")
+    async with bot.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=1959977163))
+            await bot.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("`Please unblock @decryptorxbot  and try again`")
+            return
+        if response.text.startswith("Forward"):
+            await event.edit("`can you kindly disable your forward privacy settings for good?`")
+        else:
+            await event.edit(f"{response.message.message}")
+
 
 @register(outgoing=True, pattern=r"^\.gen(?: |$)(.*)")
 async def _(event):
