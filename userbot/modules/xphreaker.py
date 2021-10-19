fcd2d39b49ed7c6e9f155c0ccd0caf46dcaa62c7
ImFine <79558474+ximfine@users.noreply.github.com>
2021-10-19 13:15:21 +0700

    Update xphreaker.py

--- userbot/modules/xphreaker.py
+++ userbot/modules/xphreaker.py
@@ -206,7 +206,7 @@ async def _(event):
     if not query:
         return await event.edit("__Silahkan masukan BIN yang mau di check!..__")
     await event.edit(f"Checking BIN {query}")
-    input = event.text.split(" ", maxsplit=1)[1]
+    input = event.text.split(" BIN > ", maxsplit=1)[1]
     url = requests.get(f"https://lookup.binlist.net/{input}")
     res = url.json()
     sys.stdout.flush()
